3acac8c2440fa5cf1a5ae4bdc33624de1709b406
Rinata Baygulova <rinulya0912@gmail.com>
2020-11-29 16:04:31 +0300

    fix: исправить ошибки при запуске

--- backend/app.js
+++ backend/app.js
@@ -35,8 +35,8 @@ app.post('/signup', celebrate({
     name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
     about: Joi.string().min(2).max(30).default('Исследователь'),
     avatar: Joi.string().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
-    email: Joi.string().unique().required(),
-    password: Joi.string().required().min(8).select(false),
+    email: Joi.string().required(),
+    password: Joi.string().required().min(8),
   }).unknown(true),
 }), createUser);
 
