ec5fcfab88ff25794579ac6d6c14f298eaf7bad4
Rinata Baygulova <rinulya0912@gmail.com>
2020-11-29 16:00:39 +0300

    feat: удалить unique

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -12,7 +12,7 @@ router.patch('/users/me', celebrate({
     name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
     about: Joi.string().min(2).max(30).default('Исследователь'),
     avatar: Joi.string().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
-    email: Joi.string().unique().required(),
+    email: Joi.string().required(),
     password: Joi.string().required().min(8).select(false),
   }).unknown(true),
 }), updateProfile);
