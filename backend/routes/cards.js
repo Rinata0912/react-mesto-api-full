fabd8c6e6fec70af9b4fe8e9aa525dcc23406e81
Rinata Baygulova <rinulya0912@gmail.com>
2020-12-03 18:12:22 +0300

    fix: добавить правки по ревью

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -17,17 +17,17 @@ router.post('/cards', celebrate({
 }), createCard);
 router.delete('/cards/:cardId', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().hex(),
+    cardId: Joi.string().hex().length(24),
   }).unknown(true),
 }), deleteCard);
 router.put('/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().hex(),
+    cardId: Joi.string().hex().length(24),
   }).unknown(true),
 }), likeCard);
 router.delete('/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().hex(),
+    cardId: Joi.string().hex().length(24),
   }).unknown(true),
 }), unlikeCard);
 
